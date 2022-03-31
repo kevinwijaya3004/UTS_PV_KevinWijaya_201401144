5f4038e772389754d7b324db64125ba9c5d8aa5b
kevinwijaya3004 <74670022+kevinwijaya3004@users.noreply.github.com>
2022-03-31 15:04:45 +0700

    Add files via upload
    UTS Pemrograman Visual

---
+++ KasirBaru/KasirBaru_Pertemuan7/Koneksi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace KasirBaru_Pertemuan7
+{
+    class Koneksi
+    {
+		public SqlConnection GetConn()
+		{
+			SqlConnection Conn = new SqlConnection();
+			Conn.ConnectionString = "Data Source=LAPTOP-TV5R5C98\\SQLEXPRESS; initial catalog=Kasir; integrated security=true";
+			return Conn;
+		}
+	}
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,52 @@
+USE [Kasir]
+GO
+
+INSERT INTO [dbo].[TBL_BARANG]
+           ([KodeBarang]
+           ,[NamaBarang]
+           ,[HargaJual]
+           ,[HargaBeli]
+           ,[JumlahBarang]
+           ,[SatuanBarang])
+     VALUES
+           ('A001'
+		   ,'Printer Epson'
+		   ,'2100000'
+		   ,'2000000'
+		   ,'10'
+		   ,'Unit')
+GO
+
+INSERT INTO [dbo].[TBL_BARANG]
+           ([KodeBarang]
+           ,[NamaBarang]
+           ,[HargaJual]
+           ,[HargaBeli]
+           ,[JumlahBarang]
+           ,[SatuanBarang])
+     VALUES
+           ('A002'
+		   ,'Printer Hp'
+		   ,'1900000'
+		   ,'1850000'
+		   ,'10'
+		   ,'Unit')
+GO
+
+INSERT INTO [dbo].[TBL_BARANG]
+           ([KodeBarang]
+           ,[NamaBarang]
+           ,[HargaJual]
+           ,[HargaBeli]
+           ,[JumlahBarang]
+           ,[SatuanBarang])
+     VALUES
+           ('A003'
+		   ,'Printer Canon'
+		   ,'1800000'
+		   ,'1700000'
+		   ,'10'
+		   ,'Unit')
+GO
+
+
